644433a40726a8b70930a18494f0a9a190796d88
itsnang <lorn.samnang.1221@rupp.edu.kh>
2024-05-28 11:49:36 +0700

    Update TelegramService with environment variables

--- lib/app/services/telegram_service.dart
+++ lib/app/services/telegram_service.dart
@@ -6,15 +6,15 @@ import 'package:teledart/telegram.dart';
 import 'package:update_her/app/services/abs_telegram.dart';
 
 class TelegramService extends AbsAnalyticServiceAdapter {
-  var BOT_TOKEN = '7101402290:AAGjxXm57K-1q8DZiXcgyDI4wZX8-peqL5A';
-  var GROUP_ID = '-4279231714';
+  var botToken = const String.fromEnvironment('BOT_TOKEN');
   late TeleDart teledart;
   @override
   Future<void> init() async {
-    final username = (await Telegram(BOT_TOKEN).getMe()).username;
-    teledart = TeleDart(BOT_TOKEN, Event(username ?? ''));
+    const groupId = String.fromEnvironment('GROUP_ID');
+    final username = (await Telegram(botToken).getMe()).username;
+    teledart = TeleDart(botToken, Event(username ?? ''));
     teledart.start();
-    await teledart.sendMessage(GROUP_ID, 'Bot started!');
+    await teledart.sendMessage(groupId, 'Bot started!');
   }
 
   @override
